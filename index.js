b4f8590229521eaaea968f8db0c6f10a26fbd98d
zhanqi <zhanqi.me@gmail.com>
2018-08-25 22:23:19 +0800

    1.3.0

--- index.js
+++ index.js
@@ -22,82 +22,11 @@ module.exports = (api, options) => {
       .use(ContextReplacementPlugin, [/moment[\/\\]locale$/, momentLocale]);
   });
 
+  api.configureWebpack(webpackConfig => {
+
+  });
+
   api.configureDevServer(devServerConfig => {
     devServerConfig.disableHostCheck = true;
   });
-
-  api.registerCommand('library', {
-    description: 'library for production',
-    usage: 'react-cli-service library [options] [entry|pattern]',
-    options: {
-      '--name': `name for lib (default: "name" in package.json or entry filename)`,
-      '--report': `generate report.html to help analyze bundle content`,
-    },
-  }, (args) => {
-    args.entry = args.entry || args._[0];
-
-    build(args, api, options);
-  });
 };
-
-async function build(args, api, options) {
-  const fs = require('fs-extra');
-  const path = require('path');
-  const webpack = require('webpack');
-  const formatStats = require('./formatStats');
-  const {
-    log,
-    logWithSpinner,
-    stopSpinner,
-  } = require('@vue/cli-shared-utils');
-
-  log();
-
-  const mode = api.service.mode;
-  logWithSpinner(`Building for ${mode} as library commonjs...`);
-
-  const targetDir = api.resolve(options.outputDir);
-
-  // resolve raw webpack config
-  let webpackConfig = require('./resolveLibConfig')(api, args, options);
-
-  if (args.report) {
-    const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-    webpackConfig.plugins.push(new BundleAnalyzerPlugin({
-      logLevel: 'warn',
-      openAnalyzer: false,
-      analyzerMode: 'static',
-      reportFilename: `report.html`,
-    }));
-  }
-
-  await fs.remove(targetDir);
-
-  return new Promise((resolve, reject) => {
-    webpack(webpackConfig, (err, stats) => {
-      stopSpinner(false);
-      if (err) {
-        return reject(err);
-      }
-
-      if (stats.hasErrors()) {
-        return reject(`Build failed with errors.`);
-      }
-
-      if (!args.silent) {
-        const targetDirShort = path.relative(
-          api.service.context,
-          targetDir,
-        );
-        log(formatStats(stats, targetDirShort, api));
-      }
-
-      resolve();
-    });
-  });
-}
-
-module.exports.defaultModes = {
-  library: 'production',
-};
-
