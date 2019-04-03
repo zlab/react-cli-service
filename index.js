8e059b77d631d72b912466f94839092d0cf21d73
zhanqi <zhanqi.me@gmail.com>
2019-04-03 15:51:41 +0800

    1.6

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 module.exports = (api, options) => {
   api.chainWebpack(chainConfig => {
-    chainConfig.plugins.delete('progress');
-    chainConfig.plugins.delete('preload');
-    chainConfig.plugins.delete('prefetch');
-
-    chainConfig.performance.hints(false);
-
     chainConfig.resolve.alias.set('src', chainConfig.resolve.alias.get('@'));
-
-    const pluginOptions = options.pluginOptions || {};
-    const { momentLocale } = pluginOptions.react || {};
-    const { ContextReplacementPlugin } = require('webpack');
-
-    momentLocale && chainConfig.plugin('context-replacement')
-      .use(ContextReplacementPlugin, [/moment[\/\\]locale$/, momentLocale]);
   });
 
   api.configureDevServer(devServerConfig => {
