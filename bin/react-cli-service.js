ad0616df35af5d1f41979f95c340e8b88f87964c
zhanqi <zhanqi.me@gmail.com>
2019-07-26 16:56:00 +0800

    1.7.6

--- bin/react-cli-service.js
+++ bin/react-cli-service.js
@@ -14,6 +14,7 @@ const service = new Service(process.cwd(), {
   plugins: [
     idToPlugin('@vue/cli-plugin-babel'),
     idToPlugin('@vue/cli-plugin-eslint'),
+    // idToPlugin('@vue/cli-plugin-typescript'),
     {
       id: 'vue-cli-plugin-react',
       apply: require('../'),
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 module.exports = (api, options) => {
-  api.chainWebpack(chainConfig => {
-    chainConfig.resolve.alias.set('src', chainConfig.resolve.alias.get('@'));
+  api.chainWebpack(config => {
+    config.resolve.alias.set('src', config.resolve.alias.get('@'));
+
+    if (options.pluginOptions && options.pluginOptions.react) {
+      const { react } = options.pluginOptions;
+      if (react.typescript) {
+        config.entry('app').clear().add('./src/main.tsx');
+      } else {
+        config.entry('app').clear().add('./src/main.js');
+      }
+    }
   });
 
-  api.configureDevServer(devServerConfig => {
-    devServerConfig.disableHostCheck = true;
+  api.configureDevServer(config => {
+    config.disableHostCheck = true;
   });
 };
